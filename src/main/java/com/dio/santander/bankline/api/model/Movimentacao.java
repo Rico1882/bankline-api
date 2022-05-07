6b5b05fac4a31df088396eb81ea4a8b870a1350d
Joao Ricardo Soares da Silva <98894124+Rico1882@users.noreply.github.com>
2022-05-07 00:33:27 -0300

    testando o banco de dados

--- src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
+++ src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
@@ -24,6 +24,9 @@ public class Movimentacao {
 	
 	@Enumerated(EnumType.STRING)
 	private MovimentacaoTipo tipo;
+	
+	@Column(name = "id_Conta")
+	private Integer idConta;
 	public Integer getId() {
 		return id;
 	}
@@ -53,6 +56,13 @@ public class Movimentacao {
 	}
 	public void setTipo(MovimentacaoTipo tipo) {
 		this.tipo = tipo;
+	}
+	public Integer getIdConta() {
+		return idConta;
+	}
+
+	public void setIdConta(Integer idConta) {
+		this.idConta = idConta;
 	}
 	
 
